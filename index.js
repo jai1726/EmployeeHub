10a792d85364ca3981e32fc29798f5726425abef
jai1726 <jagadeeshdende@gmail.com>
2024-12-07 01:08:09 +0530

    Deployment Fixes

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const server = new ApolloServer({
 
 async function startServer() {
     await server.start();
-    server.applyMiddleware({ app });
+    server.applyMiddleware({ app, path: '/graphql' });
 
     const PORT = process.env.PORT || 4000;
     app.listen(PORT, () => {
